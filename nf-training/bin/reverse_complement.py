305d87358f324f8a5da9fd3a91f7985d08c15d9d
sktrinh12 <sktrinh12@gmail.com>
2023-01-10 04:28:36 +0000

    moved data files from stepik to data folder; new rev complement py script

---
+++ nf-training/bin/reverse_complement.py
@@ -0,0 +1,23 @@
+import argparse
+from pathlib import Path
+
+compl = {'A':'T','T':'A','C':'G','G':'C'}
+TEXT = 'ATCG'
+
+def reverser(text):
+    x = ''
+    for i in range(len(text)):
+        x += compl[text[len(text) - i - 1]]
+    return x
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sq", help="The nucleic acid sequence input file path", required=True)
+    args = parser.parse_args()
+    target_dir = Path(args.sq)
+    with open(target_dir, 'r') as f:
+        content = f.read().strip()
+    #print(content)
+    res = reverser(content)
+    print(res)
